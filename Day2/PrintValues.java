8690b94f8245d1fd56acdc4c944112c52bc0b712
Sayantani Saha <104317798+Kona1246@users.noreply.github.com>
2025-07-22 22:48:23 +0530

    Add files via upload

---
+++ Day2/PrintValues.java
@@ -0,0 +1,17 @@
+package JavaCodes;
+
+public class PrintValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You passed " + args.length + " args.");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "] = " + args[i]);
+        }
+    }
+
+
+
+	}
+
+
---
+++ Day2/ReplaceSpaces.java
@@ -0,0 +1,16 @@
+package JavaCodes;
+
+public class ReplaceSpaces {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentence = "Java is fun";
+        String replaced = sentence.replace(" ", "-");
+        System.out.println("Replaced: " + replaced);
+    }
+
+
+
+	}
+
+
