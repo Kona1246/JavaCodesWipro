e69b9de6369ee0329792b3522410d81def2246b7
Sayantani Saha <104317798+Kona1246@users.noreply.github.com>
2025-07-29 01:01:30 +0530

    Update MultiThreadingTwo.java

--- Day8/MultiThreadingTwo.java
+++ Day8/MultiThreadingTwo.java
@@ -2,7 +2,7 @@ package Day8;
 class Mul3 implements Runnable{
 	public void run() {
 		for(int i=1;i<=10;i++) {
-			System.out.println("this is sai");
+			System.out.println("this is sayantani");
 			try {
 				Thread.sleep(10);
 			}
