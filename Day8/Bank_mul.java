004e4c33d4a3f2db2be44f8b95e6711baf8c8371
Sayantani Saha <104317798+Kona1246@users.noreply.github.com>
2025-07-28 16:14:05 +0530

    Add files via upload

---
+++ Day8/Bank_mul.java
@@ -0,0 +1,27 @@
+package Day8;
+
+public class Bank_mul {
+	private double balance;
+	
+	public Bank_mul(double initialBalance)
+	{
+		this.balance=initialBalance;
+	}
+	
+	public synchronized void deposit(double amount) {
+		balance+=amount;
+		System.out.printf("%s deposited: %.2f, Balance:%.2f ",Thread.currentThread().getName(),amount,balance);
+	}
+	public synchronized void withdraw(double amount) {
+		if(balance>=amount) {
+			balance-=amount;
+			System.out.printf("%s withdrew: %.2f, Balance: %.2f%n",Thread.currentThread().getName(),amount,balance);
+		} else {
+			System.out.printf("%s: Insufficient funds for withdrawing %.2f, Balance: %.2f%n",Thread.currentThread().getName(),amount,balance);
+		}
+	}
+	public double getbalance() {
+		return balance;
+	}
+
+}
