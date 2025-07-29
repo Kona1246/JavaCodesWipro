aac36fe164bf42ff1318bc39f325455c563f86a1
Sayantani Saha <104317798+Kona1246@users.noreply.github.com>
2025-07-29 09:22:24 +0530

    Add files via upload

---
+++ Course_Assessment1/EvenOdd.java
@@ -0,0 +1,31 @@
+package JavaCodesDay4;
+import java.util.Scanner;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		
+
+		   
+		        Scanner scanner = new Scanner(System.in);
+
+		        System.out.print("Enter a number: ");
+
+		        int number = scanner.nextInt();
+
+		        if (number % 2 == 0) {
+		            System.out.println(number + " is an even number.");
+		        } else {
+		            System.out.println(number + " is an odd number.");
+		        }
+
+		        scanner.close();
+		    }
+		
+
+
+	}
+
+
