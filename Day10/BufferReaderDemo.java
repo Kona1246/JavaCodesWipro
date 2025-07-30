78710cc7089466cbfbfcc81016f8ab5cb0dab6ed
Sayantani Saha <104317798+Kona1246@users.noreply.github.com>
2025-07-30 16:20:42 +0530

    Add files via upload

---
+++ Day10/BufferReaderDemo.java
@@ -0,0 +1,26 @@
+package Day10;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BufferReaderDemo {
+
+	public static void main(String[] args) throws IOException  {
+		// TODO Auto-generated method stub
+System.out.println("Enter a number: ");
+        
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println("You entered: " + a);
+
+        br.close();
+    }
+
+		
+
+	}
+
+
---
+++ Day10/ScannerDemo.java
@@ -0,0 +1,17 @@
+package Day10;
+import java.util.Scanner;
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a = sc.nextInt();
+		System.out.println("Result: " + a);
+		
+	}
+
+
+	}
+
+
